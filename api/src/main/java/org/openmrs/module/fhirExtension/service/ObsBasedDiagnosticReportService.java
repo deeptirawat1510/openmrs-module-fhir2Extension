3e71aa20e1c066ce824ebde668bdb0e00ddc68cd
pajmera4 <37437591+pajmera4@users.noreply.github.com>
2022-06-02 11:36:42 +0530

    BAH-1811 Create github actions to publish omod in nessus repository (#2)
    co-authored by amankrayush<amankumar.ayush@thoughtworks.com>

--- api/src/main/java/org/openmrs/module/fhirExtension/service/ObsBasedDiagnosticReportService.java
+++ api/src/main/java/org/openmrs/module/fhirExtension/service/ObsBasedDiagnosticReportService.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.fhirExtension.service;
 
+import ca.uhn.fhir.model.api.Include;
 import ca.uhn.fhir.rest.api.SortSpec;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.param.DateRangeParam;
@@ -8,9 +9,11 @@ import ca.uhn.fhir.rest.param.TokenAndListParam;
 import org.hl7.fhir.r4.model.DiagnosticReport;
 import org.openmrs.Obs;
 import org.openmrs.api.ObsService;
+import org.openmrs.module.fhir2.FhirConstants;
 import org.openmrs.module.fhir2.api.FhirDiagnosticReportService;
 import org.openmrs.module.fhir2.api.dao.FhirDiagnosticReportDao;
 import org.openmrs.module.fhir2.api.search.SearchQuery;
+import org.openmrs.module.fhir2.api.search.SearchQueryInclude;
 import org.openmrs.module.fhir2.api.search.param.SearchParameterMap;
 import org.openmrs.module.fhir2.api.translators.DiagnosticReportTranslator;
 import org.openmrs.module.fhir2.model.FhirDiagnosticReport;
@@ -21,10 +24,11 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toSet;
 
 @Primary
 @Component
@@ -45,11 +49,19 @@ public class ObsBasedDiagnosticReportService implements FhirDiagnosticReportServ
 	private DiagnosticReportObsValidator validator;
 	
 	@Autowired
-	private SearchQuery<FhirDiagnosticReport, DiagnosticReport, FhirDiagnosticReportDao, DiagnosticReportObsTranslator> searchQuery;
+	private SearchQuery<FhirDiagnosticReport, DiagnosticReport, FhirDiagnosticReportDao, DiagnosticReportTranslator, SearchQueryInclude<DiagnosticReport>> searchQuery;
+	
+	@Autowired
+	private SearchQueryInclude<DiagnosticReport> searchQueryInclude;
 	
 	@Override
 	public DiagnosticReport get(@Nonnull String s) {
 		return diagnosticReportObsTranslator.toFhirResource(fhirDiagnosticReportDao.get(s));
+	}
+	
+	@Override
+	public List<DiagnosticReport> get(@Nonnull Collection<String> collection) {
+		return null;
 	}
 	
 	@Override
@@ -81,19 +93,24 @@ public class ObsBasedDiagnosticReportService implements FhirDiagnosticReportServ
 	@Override
 	public IBundleProvider searchForDiagnosticReports(ReferenceAndListParam encounterReference,
 	        ReferenceAndListParam patientReference, DateRangeParam issueDate, TokenAndListParam code,
-	        ReferenceAndListParam result, TokenAndListParam id, DateRangeParam lastUpdated, SortSpec sort) {
-		SearchParameterMap theParams = (new SearchParameterMap())
-		        .addParameter("encounter.reference.search.handler", encounterReference)
-		        .addParameter("patient.reference.search.handler", patientReference)
-		        .addParameter("date.range.search.handler", issueDate).addParameter("coded.search.handler", code)
-		        .addParameter("result.search.handler", result).addParameter("common.search.handler", "_id.property", id)
-		        .addParameter("common.search.handler", "_lastUpdated.property", lastUpdated).setSortSpec(sort);
-		return this.searchQuery.getQueryResults(theParams, fhirDiagnosticReportDao, diagnosticReportObsTranslator);
+	        ReferenceAndListParam result, TokenAndListParam id, DateRangeParam lastUpdated, SortSpec sort,
+	        HashSet<Include> includes) {
+		SearchParameterMap theParams = new SearchParameterMap()
+		        .addParameter(FhirConstants.ENCOUNTER_REFERENCE_SEARCH_HANDLER, encounterReference)
+		        .addParameter(FhirConstants.PATIENT_REFERENCE_SEARCH_HANDLER, patientReference)
+		        .addParameter(FhirConstants.DATE_RANGE_SEARCH_HANDLER, issueDate)
+		        .addParameter(FhirConstants.CODED_SEARCH_HANDLER, code)
+		        .addParameter(FhirConstants.RESULT_SEARCH_HANDLER, result)
+		        .addParameter(FhirConstants.COMMON_SEARCH_HANDLER, FhirConstants.ID_PROPERTY, id)
+		        .addParameter(FhirConstants.COMMON_SEARCH_HANDLER, FhirConstants.LAST_UPDATED_PROPERTY, lastUpdated)
+		        .addParameter(FhirConstants.INCLUDE_SEARCH_HANDLER, includes).setSortSpec(sort);
+		return searchQuery.getQueryResults(theParams, fhirDiagnosticReportDao, diagnosticReportObsTranslator,
+		    searchQueryInclude);
 	}
 	
 	private Set<Obs> createObs(Set<Obs> results) {
 		return results.stream()
 				.map(obs -> obsService.saveObs(obs, SAVE_OBS_MESSAGE))
-				.collect(toSet());
+				.collect(Collectors.toSet());
 	}
 }
