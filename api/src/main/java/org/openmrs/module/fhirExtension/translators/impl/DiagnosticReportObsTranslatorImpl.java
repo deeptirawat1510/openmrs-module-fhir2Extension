779d172192fc385c040b84e75d01055b646c5455
pajmera4 <pajmera@thoughtworks.com>
2022-06-07 12:03:37 +0530

    BAH-1855 | Update. Extend the get implementation of Diagnostic Report resource
    co-authored by Aman Kumar Ayush <amankumar.ayush@thoughtworks.com>

--- api/src/main/java/org/openmrs/module/fhirExtension/translators/impl/DiagnosticReportObsTranslatorImpl.java
+++ api/src/main/java/org/openmrs/module/fhirExtension/translators/impl/DiagnosticReportObsTranslatorImpl.java
@@ -20,40 +20,57 @@ import static java.util.Collections.singletonList;
 
 @Component
 public class DiagnosticReportObsTranslatorImpl implements DiagnosticReportObsTranslator {
-	
+
 	@Autowired
 	private DiagnosticReportTranslator diagnosticReportTranslator;
-	
+
 	@Autowired
 	private DiagnosticReportObsTranslatorHelper diagnosticReportObsTranslatorHelper;
-	
+
 	@Autowired
 	private ConceptService conceptService;
 	
 	@Override
 	public DiagnosticReport toFhirResource(@Nonnull FhirDiagnosticReport fhirDiagnosticReport) {
-		return diagnosticReportTranslator.toFhirResource(fhirDiagnosticReport);
+		DiagnosticReport diagnosticReport = diagnosticReportTranslator.toFhirResource(fhirDiagnosticReport);
+		createPresentedForm(diagnosticReport, fhirDiagnosticReport);
+		return diagnosticReport;
 	}
-	
+
 	@Override
 	public FhirDiagnosticReport toOpenmrsType(@Nonnull DiagnosticReport diagnosticReport) {
 		FhirDiagnosticReport fhirDiagnosticReport = diagnosticReportTranslator.toOpenmrsType(diagnosticReport);
 		updateObsResults(fhirDiagnosticReport, diagnosticReport);
 		return fhirDiagnosticReport;
 	}
-	
+
 	private void updateObsResults(FhirDiagnosticReport fhirDiagnosticReport, DiagnosticReport diagnosticReport) {
-        if (diagnosticReport.hasPresentedForm()) {
-            Attachment labAttachment = diagnosticReport.getPresentedForm().get(0);
-            LabResult labResult = new LabResult();
-            labResult.setPatient(fhirDiagnosticReport.getSubject());
-            labResult.setObservationDate(fhirDiagnosticReport.getIssued());
-            labResult.setConcept(fhirDiagnosticReport.getCode());
-            labResult.setLabReportUrl(labAttachment.getUrl());
-            labResult.setLabReportNotes(diagnosticReport.getConclusion());
-            labResult.setLabReportFileName(labAttachment.getTitle());
-            fhirDiagnosticReport.setResults(new HashSet<>(
-                    singletonList(diagnosticReportObsTranslatorHelper.createObs(labResult))));
-        }
-    }
+		if(diagnosticReport.hasPresentedForm()){
+			Attachment labAttachment = diagnosticReport.getPresentedForm().get(0);
+			LabResult labResult = new LabResult();
+			labResult.setPatient(fhirDiagnosticReport.getSubject());
+			labResult.setObservationDate(fhirDiagnosticReport.getIssued());
+			labResult.setConcept(fhirDiagnosticReport.getCode());
+			labResult.setLabReportUrl(labAttachment.getUrl());
+			labResult.setLabReportNotes(diagnosticReport.getConclusion());
+			labResult.setLabReportFileName(labAttachment.getTitle());
+			fhirDiagnosticReport.setResults(new HashSet<>(
+					singletonList(diagnosticReportObsTranslatorHelper.createObs(labResult))));
+		}
+	}
+
+	private void createPresentedForm(DiagnosticReport diagnosticReport, FhirDiagnosticReport fhirDiagnosticReport) {
+		Iterator<Obs> obsResultsIterator = fhirDiagnosticReport.getResults().iterator();
+		if(obsResultsIterator.hasNext()){
+			Obs obsResult = obsResultsIterator.next();
+			LabResult labResult = diagnosticReportObsTranslatorHelper.getLabResult(obsResult);
+			if (labResult.isLabReportPresent()) {
+				Attachment attachment = new Attachment();
+				attachment.setUrl(labResult.getLabReportUrl());
+				attachment.setTitle(labResult.getLabReportFileName());
+				diagnosticReport.setPresentedForm(Arrays.asList(attachment));
+			}
+			diagnosticReport.setConclusion(labResult.getLabReportNotes());
+		}
+	}
 }
